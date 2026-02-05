6995684a9e2ef111b7eec88ff382332f67a388db
wz <1437175102@qq.com>
2026-02-05 22:16:10 +0800

    添加机器人类和用户玩家类

---
+++ Landlords/robot.cpp
@@ -0,0 +1,38 @@
+#include "robot.h"
+
+
+Robot::Robot(QObject *parent) : Player(parent)
+{
+    //设置玩家类型
+    m_type = Player::Robot;
+}
+
+
+
+
+void Robot::prepareCallLord()
+{
+
+}
+
+
+
+void Robot::preparePlayHand()
+{
+
+}
+
+
+
+void Robot::thinkCallLord()
+{
+
+}
+
+
+
+void Robot::thinkPlayHand()
+{
+
+}
+
